d5cc0b7240d1e163ceb1a0e92476a1662845410e
Addison Kimball <addison@akimball.com>
2026-06-29 19:11:49 -0700

    Fix playback info method for partial transcodes

--- Shared/Extensions/JellyfinAPI/SessionInfoDto.swift
+++ Shared/Extensions/JellyfinAPI/SessionInfoDto.swift
@@ -24,13 +24,14 @@ extension SessionInfoDto {
         if let transcodingInfo {
 
             let isVideoDirect = transcodingInfo.isVideoDirect ?? false
-            let hasVideoCodec = transcodingInfo.videoCodec != nil
             let isAudioDirect = transcodingInfo.isAudioDirect ?? false
 
-            if isVideoDirect || hasVideoCodec, isAudioDirect {
+            if isVideoDirect && isAudioDirect {
                 return L10n.remux
             } else if isVideoDirect {
                 return PlayMethod.directStream.displayTitle
+            } else {
+                return PlayMethod.transcode.displayTitle
             }
         }
 
